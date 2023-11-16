199d1ecb5b855b1686467672dc1fc3a0d11353b5
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-11-15 21:09:54 -0300

    Create my first component

---
+++ first-project/src/app/my-first/my-first.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-my-first',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './my-first.component.html',
+  styleUrl: './my-first.component.scss'
+})
+export class MyFirstComponent {
+
+}
