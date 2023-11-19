277bc60532e0366f482c3356743797cf1088d304
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-11-19 19:07:41 -0300

    Class and Style Binding exercising

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { DataBindingComponent } from './data-binding/data-binding.component'
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [AppComponent, DataBindingComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgbAlertModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core'
 export class DataBindingComponent {
   url: string = 'http://localhost:4200'
   angularCourse: boolean = true
-  imageUrl: string = 'http://lorempixel.com.br/400/200/'
+  imageUrl: string = 'http://lorempixel.com.br/400/200'
   imageCounter: number = 0
 
   getValue() {
