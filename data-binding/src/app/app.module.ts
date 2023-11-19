14f08d35b699668dc90efeb4b530a829f7dbbad6
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-11-19 18:21:32 -0300

    Interpolation and Property Binding exercising

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DataBindingComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.scss',
+})
+export class DataBindingComponent {
+  url: string = 'http://localhost:4200'
+  angularCourse: boolean = true
+  imageUrl: string = 'http://lorempixel.com.br/400/200/'
+  imageCounter: number = 0
+
+  getValue() {
+    return 1
+  }
+
+  getLikedCourse() {
+    return true
+  }
+
+  getImageUrl() {
+    return `${this.imageUrl}/?${++this.imageCounter}`
+  }
+}
