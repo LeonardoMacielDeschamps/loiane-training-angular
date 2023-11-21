76477e994418889b69d9358fe0c00e2a77545471
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-11-20 21:56:03 -0300

    Event Binding exercises

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,9 @@ export class DataBindingComponent {
   angularCourse: boolean = true
   imageUrl: string = 'http://lorempixel.com.br/400/200'
   imageCounter: number = 0
+  inputValue: string = ''
+  savedInputValue: string = ''
+  isMouseOver: boolean = false
 
   getValue() {
     return 1
@@ -21,5 +24,22 @@ export class DataBindingComponent {
 
   getImageUrl() {
     return `${this.imageUrl}/?${++this.imageCounter}`
+  }
+
+  onClickButton() {
+    alert('ola')
+  }
+
+  setInputValue(event: KeyboardEvent) {
+    const inputElement = event.target as HTMLInputElement
+    this.inputValue = inputElement.value
+  }
+
+  saveInputValue(inputValue: string) {
+    this.savedInputValue = inputValue
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver
   }
 }
